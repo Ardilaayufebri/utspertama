1bd090f7b947263c7d66f840b8d8a1b3f217a027
Ardilaayufebri <133196988+Ardilaayufebri@users.noreply.github.com>
2023-05-11 16:22:10 +0700

    Update Main.java

--- pengguna/Main.java
+++ pengguna/Main.java
@@ -2,7 +2,7 @@ package pengguna;
 
 public class Main {
     public static void main(String[] args) {
-        Mahasiswa mahasiswa = new Mahasiswa("username", "password", "Aji Bagus Prasetya", "G.111.19.0045", 8);
+        Mahasiswa mahasiswa = new Mahasiswa("username", "password", "Ardila Ayu Febriana", "G.111.19.0054", 8);
 
         System.out.println("Username: " + mahasiswa.getUsername());
         System.out.println("Password: " + mahasiswa.getPassword());
@@ -11,8 +11,8 @@ public class Main {
         System.out.println("SMT: " + mahasiswa.getSmt());
 
         // Mengubah nilai atribut menggunakan setter
-        mahasiswa.setNama("Aji Bagus Prasetya 0045");
-        mahasiswa.setNim("G111190045");
+        mahasiswa.setNama("Ardila Ayu Febriana");
+        mahasiswa.setNim("G111190054");
         mahasiswa.setSmt(7);
 
         System.out.println("Nama (setelah perubahan): " + mahasiswa.getNama());
