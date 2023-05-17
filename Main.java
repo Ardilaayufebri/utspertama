0d0c79ef86c94225a197d6a01827d4fee089bb9e
Ardilaayufebri <133196988+Ardilaayufebri@users.noreply.github.com>
2023-05-17 21:50:26 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import pengguna.Mhs;
+
 class Main {
     public static void main(String[] args) {
+
+        Mhs mhs = new Mhs("Ini_Username", "Ini_Password", "Ardila Ayu Febriana", "G.111.19.0054", 8);
+
+        // mhs.setNama("Ardila Ayu Febriana");
+        // mhs.setNim("G.111.19.0054");
+        // mhs.setSmt(8);
+
+        System.out.println("Nama Mhs : " + mhs.getNama());
+        System.out.println("Nim Mhs : " + mhs.getNim());
+        System.out.println("Smt Mhs : " + mhs.getSmt());
+        System.out.println("Username : " + mhs.getUsername());
+        System.out.println("Password : " + mhs.getPassword());
 
     }
 }
